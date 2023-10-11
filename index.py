79105e821ff90d007383822809eed11344b4c204
Jesús de Dios Sánchez <jesusdediossanchez@gmail.com>
2023-10-11 17:23:41 +0200

    Cambio a entorno virtual

--- index.py
+++ index.py
@@ -1,75 +1,85 @@
 import json
-import os
 import base64
-from requests import post
-
-
+from requests import post,get
 
 
 client_id = "2672e0ae0aa5490790c100b875539755"
-client_secret ="4f963dea4f53423abc1d4dc1ada3a108"
+client_secret = "4f963dea4f53423abc1d4dc1ada3a108"
 
-print(client_id,client_secret)
+print(client_id, client_secret)
+
 
 def get_token():
     auth_string = f"{client_id}:{client_secret}"
-    auth_bytes=auth_string.encode("utf-8")
-    auth_base64=str(base64.b64encode(auth_bytes),"utf-8")
-    
-    url="https://accounts.spotify.com/api/token"
-    headers={
-        "Authorization" :"Basic" +auth_base64,
-        "Content-Type ":"application/x-www-form-urlencoded"
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic" + auth_base64,
+        "Content-Type ": "application/x-www-form-urlencoded",
     }
-    data={"grant_type" : "client_credentials"}
+    data = {"grant-type": "client_credentials"}
     result = post(url, headers=headers, data=data)
-    json_result=json.loads(result.content)
-    token=json_result["access_token"]
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
     return token
 
-token=get_token()
-print(token)
-def get_auth_header(token):
-    return{"Autorizathion" :"Bearer" +token}
 
+def get_auth_header(token):
+    return {"Autorizathion": "Bearer" + token}
+
+nombre="ACDC"
 def buscar_artista(nombre):
-    url="https://api.spotify.com/v1/search"
-    headers=get_auth_header(token)
-    parametros={
-        "q":nombre,
-        "type":"artist",
-        "limit":1
-    }
+    token = get_token()
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    parametros = {"q": nombre, "type": "artist", "limit": 1}
+    parametros_url = url + parametros
+    resultado = get(parametros_url, headers=headers)
+    resultado_json = json.loads(resultado.content)
+    print(resultado_json)
+
+    """
     if name == resultado_json:
         
         return id_artista
+    """
 
-def obtenerTopTracksPorId(id_artista):    
-    topTracks=f"https://api.spotify.com/v1/artists/{id_artista}/top-tracks"
-    headers=get_auth_header(token)
-    parametros={
-        "market":"ES"
-    }
+
+token = get_token()
+
+'''
+def obtenerTopTracksPorId(id_artista):
+    topTracks = f"https://api.spotify.com/v1/artists/{id_artista}/top-tracks"
+    headers = get_auth_header(token)
+    parametros = {"market": "ES"}
     result = post(topTracks, headers=headers, data=parametros)
-    json_result=json.loads(result.content)
+    json_result = json.loads(result.content)
     return json_result["tracks"]
-salir=False
 
-def crearListaReproducción(){
-while salir==False:
-    nombre_artista=input("Dime el nombre de tus artista")
-    listaReproduccion+=obtenerTopTracksPorId(id_artista=buscar_artista(nombre_artista))
-    salir=input("¿Has terminado con tus artistas?"+"\n"+"1.Si"+"2.No").lower
-    if salir=="si" or salir=="1":
-        salir==True
-    else:
-        artistas_favoritos+=[nombre_artista]
+
+def crearListaReproducción():
+    salir = False
+    while salir == False:
+        listaReproduccion = ""
+        nombre_artista = input("Dime el nombre de tus artista")
+        listaReproduccion += obtenerTopTracksPorId(
+            id_artista=buscar_artista(nombre_artista)
+        )
+        salir = input("¿Has terminado con tus artistas?" + "\n" + "1.Si" + "2.No").lower
+        if salir == "si" or salir == "1":
+            salir == True
+        else:
+            artistas_favoritos += [nombre_artista]
     return listaReproduccion
-}
-    
+
+
+listaReproduccion = crearListaReproducción()
+
 print(listaReproduccion)
 
-''''
+"""'
 
 
 
@@ -85,4 +95,5 @@ with open(urlPaís, "w") as j:
 
 for INFO_PAÍS in fichero:
     print(INFO_PAÍS[''])
+"""
 '''
